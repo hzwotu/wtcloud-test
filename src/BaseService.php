1a7aad65322a4a4a773e0b1f3f525eddd4a54931
wujiehao <jiehao@busionline.com>
2022-09-22 16:44:54 +0800

    优化

--- src/BaseService.php
+++ src/BaseService.php
@@ -47,17 +47,17 @@ class BaseService
                 $headers[str_replace('_', '-', substr($key, 5))] = $value;
             }
             if(isset($_SERVER['HTTP_AUTHORIZATION_PLATFORM'])){
-                $header['AUTHORIZATION'] = $_SERVER['HTTP_AUTHORIZATION_PLATFORM'];
+                $headers['AUTHORIZATION'] = $_SERVER['HTTP_AUTHORIZATION_PLATFORM'];
             } elseif (isset($_SERVER['PHP_AUTH_DIGEST'])) {
-                $header['AUTHORIZATION'] = $_SERVER['PHP_AUTH_DIGEST'];
+                $headers['AUTHORIZATION'] = $_SERVER['PHP_AUTH_DIGEST'];
             } elseif (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
-                $header['AUTHORIZATION'] = base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW']);
+                $headers['AUTHORIZATION'] = base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW']);
             }
             if (isset($_SERVER['CONTENT_LENGTH'])) {
-                $header['CONTENT-LENGTH'] = $_SERVER['CONTENT_LENGTH'];
+                $headers['CONTENT-LENGTH'] = $_SERVER['CONTENT_LENGTH'];
             }
             if (isset($_SERVER['CONTENT_TYPE'])) {
-                $header['CONTENT-TYPE'] = $_SERVER['CONTENT_TYPE'];
+                $headers['CONTENT-TYPE'] = $_SERVER['CONTENT_TYPE'];
             }
         }
         return $headers;
